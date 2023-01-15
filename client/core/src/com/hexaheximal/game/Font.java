8ebd7d7077e93ac5f31625b0da32208ab94a0968
hexaheximal <hexaheximal@proton.me>
2023-01-14 19:13:55 -0500

    Initial commit

---
+++ client/core/src/com/hexaheximal/game/Font.java
@@ -0,0 +1,37 @@
+package com.hexaheximal.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.utils.Align;
+
+public class Font {
+	public BitmapFont font;
+	public int size;
+	
+	public Font(String filename, int size) {
+		this.size = size;
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(filename));
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		parameter.size = this.size;
+		parameter.characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"´`'<>";
+		this.font = generator.generateFont(parameter);
+		generator.dispose();
+	}
+	
+	public void render(SpriteBatch batch, float x, float y, String text, boolean centered) {
+		GlyphLayout glyphLayout = new GlyphLayout();
+		glyphLayout.setText(this.font, text);
+		float w = glyphLayout.width;
+		float h = glyphLayout.height;
+		if (centered) {
+			this.font.draw(batch, glyphLayout, x - (w / 2), y - (h / 2));
+		}
+		else {
+			this.font.draw(batch, glyphLayout, x, y);
+		}
+	}
+}
---
+++ client/desktop/src/com/hexaheximal/game/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.hexaheximal.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.hexaheximal.game.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Game");
+		config.setWindowedMode(1920, 1080);
+		config.useVsync(true);
+		new Lwjgl3Application(new Game(System.getProperty("os.name"), "Unknown"), config);
+	}
+}
